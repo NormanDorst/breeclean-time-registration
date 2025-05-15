d387feafc92e7ec5de648336271a4aa0aca030b7
Norman <normandorst@hotmail.com>
2025-05-15 22:44:40 +0200

    gewoon goed

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/breeclean-time-registration/", // let op de slash aan het begin én einde
+  base: "/breeclean-time-registration/",
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
 });
